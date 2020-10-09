22c3ba49dc860e4b58910096ea5eb733b01da3ab
Daniel <daniel.lee06@outlook.com>
2020-10-08 20:32:53 -0400

    minor change in delay code

--- question-2/callbacks.js
+++ question-2/callbacks.js
@@ -1,21 +1,21 @@
 function resolvedPromise() {
-    const promise = new Promise((resolve) => {
-        resolve({
-            'message': 'delayed success!'
-        });
-    })
-    setTimeout(() =>
-        promise.then(message => console.log(message)), 500);
+  const promise = new Promise((resolve) => {
+  setTimeout(() =>
+    resolve({
+      'message': 'delayed success!'
+    }, 500));
+  })
+    promise.then(message => console.log(message));
 }
 
 function rejectedPromise() {
-    const promise = new Promise((resolve, reject) => {
-        reject({
-            'error': 'delayed exception!'
-        });
-    })
-    setTimeout(() =>
-        promise.catch(message => console.log(message)), 500);
+  const promise = new Promise((resolve, reject) => {
+  setTimeout(() =>
+    reject({
+      'error': 'delayed exception!'
+    }, 500));
+  })
+    promise.catch(message => console.log(message));
 }
 
 resolvedPromise();
